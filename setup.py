24225866d8467d20880488f7a8893c40857af97e
Khaled <khalegjr@gmail.com>
2022-12-28 18:52:50 -0300

    Set up project initial organization and metadata
    - Add setup.py to turn installable
    - Add __main__ for executable
    - Add __init__ for package
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dundie',
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Júnior Khaled",
+    packages=find_packages()
+)
