eedbd621ebb22a62e3de060a32c4291fa36d91c7
Khaled <khalegjr@gmail.com>
2022-12-28 20:21:45 -0300

    create basic cli subcommands and arguments

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,5 +1,26 @@
+import argparse
+
 
 def main():
+
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewards CLI",
+        epilog="Enjoy and use with cautious.",
+    )
+    parser.add_argument(
+        "subcomand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load", "show", "send")
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+    )
+
+    args = parser.parse_args()
+
     print("Executing entry point for dundie...")
 
 
