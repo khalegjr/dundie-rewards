39cf4f6970e1260291b8d67c5a350eedca4c98e5
Khaled <khalegjr@gmail.com>
2022-12-28 19:38:20 -0300

    turned to installable binary #9

---
+++ dundie/__main__.py
@@ -0,0 +1,4 @@
+
+
+def main():
+    print("Executing entry point for dundie...")
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='dundie',
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Júnior Khaled",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    }
+)
