77d1ce0f89d2be9df9601fc745f45c8a03f7383f
Khaled <khalegjr@gmail.com>
2023-03-28 14:42:40 -0300

    Cria teste de integração Fixes #1

---
+++ integration/test_load.py
@@ -0,0 +1,10 @@
+from subprocess import check_output
+
+
+def test_load():
+    """test command load"""
+    out = check_output(
+        ["dundie", "load", "tests/assets/people.csv"]
+    ).decode('utf-8').split("\n")
+    # breakpoint()
+    assert len(out) == 2
