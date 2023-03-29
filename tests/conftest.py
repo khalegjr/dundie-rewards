36b1681006b408b068782bc9d6829f71d5574a1c
Khaled <khalegjr@gmail.com>
2023-03-29 15:29:08 -0300

    cria fixture do pytest
    muda a pasta onde os testes são executados, para evitar criação de lixo na pasta principal

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 MARKER = """\
 unit: Mark unit tests
 integration: Mark integration tests
@@ -11,3 +13,11 @@ def pytest_configure(config):
     # modo de execução interativo
     for line in MARKER.split("\n"):
         config.addinivalue_line('markers', line)
+
+
+@pytest.fixture(autouse=True)
+def go_to_tmp_dir(request):
+    tmpdir = request.getfixturevalue("tmpdir")
+
+    with tmpdir.as_cwd():
+        yield
