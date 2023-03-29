bc1eef705e4fe8e7c68fd2716a44a7df320c1099
Khaled <khalegjr@gmail.com>
2023-03-29 15:50:27 -0300

    cria teste de integração para erro de comando

--- integration/test_load.py
+++ integration/test_load.py
@@ -5,22 +5,23 @@ import pytest
 
 @pytest.mark.integration
 @pytest.mark.medium
-def test_load():
+def test_load_positive_call_load_command():
     """test command load"""
-    out = check_output(
-        ["dundie", "load", "tests/assets/people.csv"]
-    ).decode('utf-8').split("\n")
+    out = (
+        check_output(["dundie", "load", "tests/assets/people.csv"])
+        .decode("utf-8")
+        .split("\n")
+    )
     # breakpoint()
     assert len(out) == 2
 
 
 @pytest.mark.integration
 @pytest.mark.medium
-@pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
-def test_load_negative_call_load_command_with_wrong_params(wrong_command):
+def test_load_negative_call_load_command_with_wrong_params():
     """test command load"""
     with pytest.raises(CalledProcessError) as error:
-        check_output(
-            ["dundie", wrong_command, "tests/assets/people.csv"]
-        ).decode('utf-8').split("\n")
+        check_output(["dundie", "loady", "tests/assets/people.csv"]).decode(
+            "utf-8"
+        ).split("\n")
         assert "status 2" in str(error)
