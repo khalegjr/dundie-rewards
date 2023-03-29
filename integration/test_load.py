b2a24b354807413abe6637278bd41d951043e6eb
Júnior Moreira de Souza <35576198+khalegjr@users.noreply.github.com>
2023-03-29 19:10:49 -0300

    Boas praticas testes (#16)
    * cria fixture do pytest
    
    muda a pasta onde os testes são executados, para evitar criação de lixo na pasta principal
    
    * separa os asserts em testes unitários próprios
    
    * cria relatório de testes no padrão junit no CI
    
    * cria teste de integração com parametrize

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,4 +1,4 @@
-from subprocess import check_output
+from subprocess import CalledProcessError, check_output
 
 import pytest
 
@@ -12,3 +12,15 @@ def test_load():
     ).decode('utf-8').split("\n")
     # breakpoint()
     assert len(out) == 2
+
+
+@pytest.mark.integration
+@pytest.mark.medium
+@pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
+def test_load_negative_call_load_command_with_wrong_params(wrong_command):
+    """test command load"""
+    with pytest.raises(CalledProcessError) as error:
+        check_output(
+            ["dundie", wrong_command, "tests/assets/people.csv"]
+        ).decode('utf-8').split("\n")
+        assert "status 2" in str(error)
