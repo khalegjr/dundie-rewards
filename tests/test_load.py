41e490157f7c4e820b247b921b9cee62a4e23f8e
Khaled <khalegjr@gmail.com>
2023-03-29 15:29:48 -0300

    separa os asserts em testes unitários próprios

--- tests/test_load.py
+++ tests/test_load.py
@@ -7,7 +7,13 @@ from .constants import PEOPLE_FILE
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load_positive_has_2_people():
     """Test load function"""
     assert len(load(PEOPLE_FILE)) == 2
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_postive_first_name_starts_with_j():
+    """Test load function"""
     assert load(PEOPLE_FILE)[0][0] == 'J'
