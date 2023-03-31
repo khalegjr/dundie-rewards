2c01dc284818dfa1bfb3b7422d9cab88aa12ee11
Khaled <khalegjr@gmail.com>
2023-03-31 01:40:49 -0300

    refac: instala pacote rich para estilizar o CLI

--- dundie/cli.py
+++ dundie/cli.py
@@ -1,7 +1,17 @@
-import click
 import pkg_resources
+import rich_click as click
+from rich import print
+from rich.console import Console
+from rich.table import Table
 
 from dundie import core
+
+click.rich_click.USE_RICH_MARKUP = True
+click.rich_click.USE_MARKDOWN = True
+click.rich_click.SHOW_ARGUMENTS = True
+click.rich_click.GROUP_ARGUMENTS_OPTIONS = True
+click.rich_click.SHOW_METAVARS_COLUMN = False
+click.rich_click.APPEND_METAVARS_HELP = True
 
 
 @click.group()
@@ -16,11 +26,20 @@ def main():
 @main.command()
 @click.argument("filepath", type=click.Path(exists=True))
 def load(filepath):
-    """Loads the file to the database."""
-    result = core.load(filepath)
-    header = ["name", "dept", "role", "e-mail"]
+    """Loads the file to the database.
 
+    - Validates data
+    - Parses the file
+    - Loads to database
+    """
+    table = Table(title="Dunder Mifflin Associates")
+    headers = ["name", "dept", "role", "e-mail"]
+    for header in headers:
+        table.add_column(header, style="magenta")
+
+    result = core.load(filepath)
     for person in result:
-        print("-" * 50)
-        for key, value in zip(header, person.split(",")):
-            print(f"{key.strip()} -> {value.strip()}")
+        table.add_row(*[field.strip() for field in person.split(",")])
+
+    console = Console()
+    console.print(table)
